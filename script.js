aea546d24afbdc23921b2fcea1a9f2746629c7a7
Mahdi <mahdihsoumi01@gmail.com>
2018-04-20 17:40:26 +0200

    daza

--- script.js
+++ script.js
@@ -6,14 +6,8 @@ button.addEventListener("click", function() {
 
 
 	if(birth.value) {
-		var birth=String(birth.value);
-		var dayB=birth.charAt(8)+birth.charAt(9);
-		var monthB=birth.charAt(5)+birth.charAt(6);
-		var yearB=birth.charAt(0)+birth.charAt(1)+birth.charAt(2)+birth.charAt(3);
-		var dateString = yearB+"/"+monthB+"/"+dayB;
-		var d = new Date(dateString);
-		var nb = (((date-d)*(0.0000000115741))*17).toFixed(0);
-		document.querySelector("h2").innerText = "  يجب عليك أن تصلي "+nb+" ركعة";
+		var nb=parseInt(((new Date().getTime()-new Date(birth.value.toString()).getTime())/86400000) * 17) ;
+		document.querySelector("p").innerText = "  يجب عليك أن تصلي "+nb+" ركعة";
 		
 	}
 		
